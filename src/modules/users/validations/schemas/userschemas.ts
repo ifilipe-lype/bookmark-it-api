4048422c32e0b7541e772be33bfb4af500068ed5
ifilipe-lype <filipe7lype@gmail.com>
2022-02-09 10:44:15 +0100

    feat: creates user's data validation

---
+++ src/modules/users/validations/schemas/userschemas.ts
@@ -0,0 +1,21 @@
+import { ICreateUserDTO } from '@modules/users/dtos/IUser';
+import * as yup from 'yup';
+
+export const createUserSchema: yup.SchemaOf<ICreateUserDTO> = yup.object().shape({
+  name: yup
+    .string()
+    .required()
+    .min(3, 'name must be longer than 3 characters!')
+    .max(255, "name can't be longer than 255 characters!"),
+  username: yup
+    .string()
+    .notRequired()
+    .min(3, 'username must be longer than 3 characters!')
+    .max(255, "username can't be longer than 255 characters!"),
+  email: yup.string().required().email('Invalid email!'),
+  email_verified: yup.boolean().default(false),
+  avatar_url: yup.string().url("Invalid avatar's url").default(null),
+  //password: yup.string().min(8, 'Password must be at least 8 chars long!'),
+  provider: yup.string().default(null),
+  provider_user_id: yup.string().default(null),
+});
