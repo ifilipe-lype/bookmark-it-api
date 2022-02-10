322d416dad2c72d748e6dfd7c42caeaf117cb664
ifilipe-lype <filipe7lype@gmail.com>
2022-02-11 00:52:15 +0100

    fix: renames file

--- src/__tests__/fixtures/containers/index.ts
+++ src/__tests__/fixtures/containers/index.ts
@@ -1,6 +1,6 @@
 import { container } from 'tsyringe';
 
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
-import { UsersRepositoryInMemory } from '../repositories/UsersRepository';
+import { UsersRepositoryInMemory } from '../repositories/UsersRepositoryInMemory';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepositoryInMemory);
